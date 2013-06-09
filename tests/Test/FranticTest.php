5246c90704ca99d31cc1552563d2d997d6506156
Glendon Solsberry <gms8994@dp.cx>
2013-06-09 19:23:43 -0400

    Add tests

---
+++ tests/Test/FranticTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Frantic;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Frantic\Frantic;
+
+echo 'Frantic ' . \Frantic::VERSION . ' tested with ';
+
+class FranticTest extends \PHPUnit_Framework_TestCase {
+
+}

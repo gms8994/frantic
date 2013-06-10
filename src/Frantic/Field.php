05b4e93247208a234b7d0f73af0ec89f09d174f4
Glendon Solsberry <gsolsberry@learninghouse.com>
2013-06-10 09:18:01 -0400

    Create getter/setter for field attrs

--- src/Frantic/Field.php
+++ src/Frantic/Field.php
@@ -9,6 +9,99 @@ class Field {
 	private $label = null;
 	private $validation = array();
 
+	/**
+	 * Getter for name
+	 *
+	 * @return mixed
+	 */
+	public function getName()
+	{
+	    return $this->name;
+	}
+
+	/**
+	 * Setter for name
+	 *
+	 * @param mixed $name Value to set
+
+	 * @return self
+	 */
+	public function setName($name)
+	{
+	    $this->name = $name;
+	    return $this;
+	}
+
+	/**
+	 * Getter for type
+	 *
+	 * @return mixed
+	 */
+	public function getType()
+	{
+	    return $this->type;
+	}
+	
+	/**
+	 * Setter for type
+	 *
+	 * @param mixed $type Value to set
+	
+	 * @return self
+	 */
+	public function setType($type)
+	{
+	    $this->type = $type;
+	    return $this;
+	}
+	
+	/**
+	 * Getter for label
+	 *
+	 * @return mixed
+	 */
+	public function getLabel()
+	{
+	    return $this->label;
+	}
+	
+	/**
+	 * Setter for label
+	 *
+	 * @param mixed $label Value to set
+	
+	 * @return self
+	 */
+	public function setLabel($label)
+	{
+	    $this->label = $label;
+	    return $this;
+	}
+	
+	/**
+	 * Getter for validation
+	 *
+	 * @return mixed
+	 */
+	public function getValidation()
+	{
+	    return $this->validation;
+	}
+	
+	/**
+	 * Setter for validation
+	 *
+	 * @param mixed $validation Value to set
+	
+	 * @return self
+	 */
+	public function setValidation($validation)
+	{
+	    $this->validation = $validation;
+	    return $this;
+	}
+	
+
 	public function create($name, $type = 'text', $label = null, $validation = array()) {
 		$this->name = $name;
 		$this->type = $type;
